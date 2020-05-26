d59d8ecfd73a06992e3df3fb3d7ec607a0cf0969
Charlton Trezevant <ct@ctis.me>
2020-05-25 23:30:30 -0400

    Implement release_courses (untested)

--- Assignment1/src/assignment1_main.c
+++ Assignment1/src/assignment1_main.c
@@ -237,26 +237,26 @@ void process_courses(course *courses, int num_courses){
    courses of a test case and also takes the size of the array. Then it free up
    all the memory allocated within it. You can create more function as needed to
    ease the process.
-*/
+ */
 void release_courses(course *courses, int num_courses){
         for(int i = 0; i < num_courses; i++) {
                 for(int j = 0; j < courses[i].num_sections; j++) {
                         int curSect_numStudents = courses[i].num_students[j];
 
                         for(int k = 0; k < curSect_numStudents; k++) {
-                                mmgr_free(global_MEM, courses[i].sections[k]->scores);
-                                mmgr_free(global_MEM, courses[i].sections[k]->lname);
-                                mmgr_free(global_MEM, courses[i].sections[k]);
+                                mmgr_free(g_MEM, courses[i].sections[k]->scores);
+                                mmgr_free(g_MEM, courses[i].sections[k]->lname);
+                                mmgr_free(g_MEM, courses[i].sections[k]);
                         }
 
-                        mmgr_free(global_MEM, courses[i].sections);
-                        mmgr_free(global_MEM, courses[i].course_name);
-                        mmgr_free(global_MEM, courses[i].num_students);
-                        mmgr_free(global_MEM, courses[i].num_scores);
+                        mmgr_free(g_MEM, courses[i].sections);
+                        mmgr_free(g_MEM, courses[i].course_name);
+                        mmgr_free(g_MEM, courses[i].num_students);
+                        mmgr_free(g_MEM, courses[i].num_scores);
                 }
         }
 
-        mmgr_free(global_MEM, courses);
+        mmgr_free(g_MEM, courses);
 }
 
 ////////////////////////// Memory manager //////////////////////////
