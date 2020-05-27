ebd98a1634599f100e8b5ba5eb7fc258f2ba12e2
Charlton Trezevant <ct@ctis.me>
2020-05-26 21:18:56 -0400

    Update mmgr redistributable v0.01

--- mmgr.c
+++ mmgr.c
@@ -1,37 +1,40 @@
-////////////////////////// Memory manager //////////////////////////
-
-// This is the initial version of my C memory manager.
-// At the moment it's very rudimentary.
-
 #include  <stdlib.h>
-#include  <stdio.h>
 
-#define DEBUG_LEVEL_MMGR 0
-#define DEBUG 0
+// debugf.h
+// (c) Charlton Trezevant - 2018
+#define DEBUG_LEVEL_ALL 0
+#define DEBUG_LEVEL_MMGR 1
+#define DEBUG_LEVEL_LOGIC 2
+#define DEBUG_LEVEL_STATE 3
+#define DEBUG_LEVEL_INFO 4
+#define DEBUG_LEVEL_NONE 99
+
+//#define DEBUG DEBUG_LEVEL_NONE
+
 #ifdef DEBUG
-#define debugf(lvl, fmt, ...)                           \
-        ({                                              \
-                if (DEBUG == 0 || (lvl) >= DEBUG) {     \
+#define debugf(lvl, fmt, ...) \
+        ({ \
+                if (DEBUG == 0 || (lvl) >= DEBUG) { \
                         fprintf(stderr, fmt, ## __VA_ARGS__); fflush(stderr); \
-                }                                       \
+                } \
         })
 #else
   #define debugf(lvl, fmt, ...) ((void)0)
 #endif
 
-// MMGR v0.01
-// (c) Charlton Trezevant 2020
+// MMGR Type definitions/function prototypes
+// v 0.01 - (c) Charlton Trezevant 2020
 // Rave reviews:
 // 10/10
 //    - IGN
-// ==3291== All heap blocks were freed -- no leaks are possible
+// All heap blocks were freed -- no leaks are possible
 //    - Valgrind
-// ~ it's pronounced "mumger" ~
+// it's pronounced "mugger"
 typedef struct MMGR_Entry {
         void* handle;
         size_t size;
-        // todo: overhaul for gc via refcounts or mark+sweep
-        // consider implementing marking/affinities for batched release
+        // todo: gc via refcounts or mark+sweep
+        // allow entries to be marked/given affinities for batched release
 } MMGR_Entry;
 
 typedef struct MMGR {
@@ -50,13 +53,23 @@ void mmgr_cleanup(MMGR *tbl);
 void mmgr_mutex_acquire(MMGR *tbl);
 void mmgr_mutex_release(MMGR *tbl);
 
+// Global MMGR instance
+MMGR* g_MEM;
+
+////////////////////////// Memory manager //////////////////////////
+
+// This is the initial version of my C memory manager.
+// At the moment it's very rudimentary.
+
 // Initializes the memory manager's global state table. This tracks all allocated
 // memory, reallocates freed entries, and ensures that all allocated memory is
 // completely cleaned up on program termination.
-// In the future, I'll spawn a worker thread to performs automated garbage
-// collection through mark and sweep or simple refcounting.
+
+// In the future, MMGR will spawn a worker thread to perform automated garbage
+// collection through mark and sweep or simple refcounting. It'll also be
+// refactored to utilize hashmaps once I figure out how to hash pointers.
 MMGR *mmgr_init(){
-        MMGR *state_table = malloc(sizeof(MMGR));
+        MMGR *state_table = calloc(1, sizeof(MMGR));
 
         state_table->free = NULL;
         state_table->numFree = 0;
@@ -75,23 +88,31 @@ MMGR *mmgr_init(){
 // If any previously allocated entries have since been freed, these will be
 // resized and reallocated to serve the request
 void *mmgr_malloc(MMGR *tbl, size_t size){
+        if(tbl == NULL)
+                return NULL;
+
+        // State table access is atomic. You'll see this around a lot.
         mmgr_mutex_acquire(tbl);
 
-        MMGR_Entry* target;
+        void* handle = NULL;
 
         // If freed entries are available, we can recycle them
         if(tbl->numFree > 0) {
+                // Fetch index of last reallocatable table entry
                 int tgt_idx = tbl->free[tbl->numFree - 1];
 
-                debugf(DEBUG_LEVEL_MMGR, "mmgr: found reusable previously allocated entry %p\n", tbl->entries[tgt_idx]);
+                debugf(DEBUG_LEVEL_MMGR, "mmgr: found reusable previously allocated entry %d\n", tgt_idx);
 
-                target = (MMGR_Entry*) realloc(tbl->entries[tgt_idx], sizeof(MMGR_Entry) + size);
-                target->size = size;
+                // Update table entry with new pointer and size, memset to 0
+                tbl->entries[tgt_idx]->size = size;
+                tbl->entries[tgt_idx]->handle = malloc(size);
+                memset(tbl->entries[tgt_idx]->handle, 0, size);
+                
+                // Copy freshly allocated region pointer to handle
+                handle = tbl->entries[tgt_idx]->handle;
 
-                tbl->entries[tgt_idx] = target;
-
+                // Resize free index array
                 tbl->free = (int*) realloc(tbl->free, (sizeof(int) * (tbl->numFree - 1)));
-
                 tbl->numFree--;
 
                 debugf(DEBUG_LEVEL_MMGR, "mmgr: reallocated %lu bytes\n", size);
@@ -101,58 +122,71 @@ void *mmgr_malloc(MMGR *tbl, size_t size){
         } else {
                 debugf(DEBUG_LEVEL_MMGR, "mmgr: no recyclable entries available, increasing table size\n");
 
+                // Resize occupied entry table to accommodate an additional entry
                 tbl->entries = (MMGR_Entry**) realloc(tbl->entries, (sizeof(MMGR_Entry*) * (tbl->numEntries + 1)));
 
-                tbl->entries[tbl->numEntries] = (MMGR_Entry*) malloc(sizeof(MMGR_Entry) + size);
+                // Allocate a new entry to include in the state table
+                tbl->entries[tbl->numEntries] = (MMGR_Entry*) calloc(1, sizeof(MMGR_Entry) + sizeof(void*));
 
-                target = tbl->entries[tbl->numEntries];
+                tbl->entries[tbl->numEntries]->handle = malloc(size);
+                memset(tbl->entries[tbl->numEntries]->handle, 0, size);
 
-                target->handle = malloc(size);
-                target->size = size;
+                // Copy freshly allocated region pointer to handle
+                handle = tbl->entries[tbl->numEntries]->handle;
+                tbl->entries[tbl->numEntries]->size = size;
 
                 tbl->numEntries++;
 
-                debugf(DEBUG_LEVEL_MMGR, "mmgr: allocated %lu bytes, handle is %p\n", target->size, target->handle);
+                debugf(DEBUG_LEVEL_MMGR, "mmgr: allocated %lu bytes, handle is %p\n", size, handle);
         }
 
         mmgr_mutex_release(tbl);
 
-        return target->handle;
+        return handle;
 }
 
-// Frees the provided pointer and checks out the active entry in the g_MEM
+
+// Frees the provided pointer and checks out the active entry in the global
 // state table so that it can be reallocated
 void mmgr_free(MMGR *tbl, void* handle){
+        if(tbl == NULL)
+                return;
+
+        if(handle == NULL) {
+                debugf(DEBUG_LEVEL_MMGR, "mmgr: provided NULL handle! no-op\n");
+                return;
+        }
+
         mmgr_mutex_acquire(tbl);
 
+        // Whether the provided pointer exists in the MMGR state table
         int found = 0;
 
         debugf(DEBUG_LEVEL_MMGR, "mmgr: num active entries is %d, called to free %p\n", (tbl->numEntries - tbl->numFree), handle);
 
+        // Search backwards through the state table for the provided pointer
+        // The idea being that the memory you allocate the last will be the first
+        // you want to free (e.g. struct members, then structs)
+        // Eventually this will be overhauled with hash maps
         for(int i = tbl->numEntries; i--> 0; ) {
-                if(handle == NULL) {
-                        debugf(DEBUG_LEVEL_MMGR, "mmgr: provided NULL handle! no-op\n");
-                        break;
-                }
-
                 if(tbl->entries[i]->handle == handle) {
                         debugf(DEBUG_LEVEL_MMGR, "mmgr: found handle %p at index %d\n", handle, i);
 
-                        MMGR_Entry* target = tbl->entries[i];
-
                         tbl->numFree++;
+                        
+                        // Free and nullify table entry
+                        free(tbl->entries[i]->handle);
+                        tbl->entries[i]->handle = NULL;
+                        tbl->entries[i]->size = 0;
 
-                        free(target->handle);
-
-                        target->size = 0;
-
+                        // Resize free entry list, ad freed entry
                         tbl->free = (int*) realloc(tbl->free, (sizeof(int) * (tbl->numFree + 1)));
-                        tbl->free[tbl->numFree] = i;
+                        tbl->free[tbl->numFree - 1] = i;
 
                         found = 1;
 
-                        debugf(DEBUG_LEVEL_MMGR, "mmgr: freed %p, %d entries remain active\n", handle, (tbl->numEntries - tbl->numFree));
-                        break;
+                        debugf(DEBUG_LEVEL_MMGR, "mmgr: freed %p at index %d, %d entries remain active\n", handle, i, (tbl->numEntries - tbl->numFree));
+
                 }
         }
 
@@ -164,26 +198,34 @@ void mmgr_free(MMGR *tbl, void* handle){
 
 // Cleans up any remaining allocated memory, then frees the state table
 void mmgr_cleanup(MMGR *tbl){
+        if(tbl == NULL)
+                return;
+
         mmgr_mutex_acquire(tbl);
 
         int deEn = 0;
 
-        debugf(DEBUG_LEVEL_MMGR, "mmgr: cleaning up\n");
+        debugf(DEBUG_LEVEL_MMGR, "mmgr: cleaning up %d active entries\n", (tbl->numEntries - tbl->numFree));
 
         // Free all active entries and what they're pointing to
         for(int i = 0; i < tbl->numEntries; i++) {
                 if(tbl->entries[i] != NULL) {
-                        free(tbl->entries[i]->handle);
+                        if(tbl->entries[i]->handle != NULL)
+                                free(tbl->entries[i]->handle);
+
                         free(tbl->entries[i]);
                         deEn++;
                 }
         }
 
-        debugf(DEBUG_LEVEL_MMGR, "mmgr: cleanup deallocd %d of %d active entries\n", deEn, (tbl->numEntries - tbl->numFree));
+        debugf(DEBUG_LEVEL_MMGR, "mmgr: cleanup deallocd %d entries\n", deEn);
 
+        // Deallocate occupied + free table entries, table itself
         free(tbl->entries);
         free(tbl->free);
         free(tbl);
+
+        tbl = NULL;
 }
 
 // Mutex acquisition/release helpers to atomicize access to the memory manager's
