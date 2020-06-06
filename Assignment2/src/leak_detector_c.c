406e83410c2cf64361a7d87d9fd12256b004c9ea
Charlton Trezevant <ct@ctis.me>
2020-06-06 12:23:57 -0400

    Remove unused variable in Ahmed's leak detector

--- Assignment2/src/leak_detector_c.c
+++ Assignment2/src/leak_detector_c.c
@@ -172,7 +172,7 @@ void remove_mem_info (void * mem_ref)
  */
 void report_mem_leak(void)
 {
-	unsigned short index;
+	//unsigned short index;
 	MEM_LEAK * leak_info;
 
 	FILE * fp_write = fopen (OUTPUT_FILE, "wt");
