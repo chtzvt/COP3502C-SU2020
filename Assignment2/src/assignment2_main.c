8f0cc7297e0f73ebf1d07ad2c958daa4f5bdbeb0
Charlton Trezevant <ct@ctis.me>
2020-06-06 12:07:59 -0400

    Add default configuration values

--- Assignment2/src/assignment2_main.c
+++ Assignment2/src/assignment2_main.c
@@ -4,17 +4,25 @@
 #include <stdarg.h>
 #include "leak_detector_c.h"
 
+// Configuration
+#define CONFIG_INFILE_NAME "in.txt"
+#define CONFIG_OUTFILE_NAME "out.txt"
+#define CONFIG_NUM_LANES 12
+#define CONFIG_MAX_TEST_CASES 25
+#define CONFIG_MAX_CUSTOMERS 500000
+#define CONFIG_MAX_CUST_ITEMS 100
+#define CONFIG_MAX_NAME_LEN 10
+#define CONFIG_MAX_TIME 1000000000
+
 // debugf.h
 // (c) Charlton Trezevant - 2018
 #define DEBUG_LEVEL_ALL 0
 #define DEBUG_LEVEL_MMGR 1
-#define DEBUG_LEVEL_LOGIC 2
-#define DEBUG_LEVEL_STATE 3
-#define DEBUG_LEVEL_INFO 4
-#define DEBUG_LEVEL_NONE 99
+#define DEBUG_LEVEL_TRACE 2
+#define DEBUG_LEVEL_INFO 3
+#define DEBUG_LEVEL_NONE 1000
 
-//#define DEBUG DEBUG_LEVEL_NONE
-
+//#define DEBUG DEBUG_LEVEL_ALL
 #ifdef DEBUG
 #define debugf(lvl, fmt, ...) \
         ({ \
